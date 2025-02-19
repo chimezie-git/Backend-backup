d222548789479869fefd7aa9bb3f168e56566415
musfirbaig <musfirbaig27june@gmail.com>
2025-02-19 20:15:15 +0500

    now checking if its issue with db - so sqlite

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -5,16 +5,41 @@ from .common import *
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = False
 
-ALLOWED_HOSTS = [".onrender.com",]
+ALLOWED_HOSTS = [".onrender.com",
+                 '127.0.0.1',
+    'localhost',  # Optional, but recommended
+                 ]
 
 CSRF_TRUSTED_ORIGINS = ["https://nitrobills-backend-backup.onrender.com",]
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # default= os.getenv("POSTGRESS_DATABASE_URL"),
+#         default= "postgresql://nitrobills_postgre_user:900e8zPvhntNkBIkYII01pBq17KyO2JP@dpg-cuq5ka23esus738kjepg-a.oregon-postgres.render.com/nitrobills_postgre",
+#         conn_max_age=600
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default= os.getenv("POSTGRESS_DATABASE_URL"),
-        conn_max_age=600
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default="postgresql://nitrobills_postgre_user:900e8zPvhntNkBIkYII01pBq17KyO2JP@dpg-cuq5ka23esus738kjepg-a.oregon-postgres.render.com/nitrobills_postgre",
+#         conn_max_age=600,
+#     )
+# }
+
+# DATABASES = {
+#     'default': dj_database_url.config(conn_max_age=600)
+# }
+
+
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.hostinger.com"
