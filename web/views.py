e6ff77b3b950e3507cf6658e1ac7750c115b037d
musfirbaig <musfirbaig27june@gmail.com>
2025-02-19 12:56:29 +0500

    server 500 error issue fixed

--- web/views.py
+++ web/views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# def home(request):
+#     return render(request, "home.html")
+
+
+from django.http import HttpResponse
+
 def home(request):
-    return render(request, "home.html")
+    html_content = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Home</title>
+    </head>
+    <body>
+        <h1>Welcome to the Home Page</h1>
+        <a href="https://nitrobills-backend-backup.onrender.com/api/docs/">Go to the Api Docs</a>
+        </br>
+        <a href="https://nitrobills-backend-backup.onrender.com/api/schema">Download Api Schema</a>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_content)
